bf4be9a8f4a1f26c9585bd1092708a99b55b5b1e
Ali Akber <94167678+aliakberi41@users.noreply.github.com>
2023-12-26 20:12:48 +0500

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+print("My name is Ali Akber")
+mySuperMessage="This is my first python program"
+print(mySuperMessage)
+mySuperMessage="This is the best and fastest python course in the world"
+print(mySuperMessage)
+
+# Finding error and reading traceback in Python
+# 1-message ="ali"
+# message 1= "akber" (these two syntaxes are not allowed to declare a variable in python)
+# also be careful in spelling
