3f527bbd5d81b1eaa93cd7a4359d0ee04487e7e2
Ali Akber <94167678+aliakberi41@users.noreply.github.com>
2023-12-27 21:07:09 +0500

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,20 @@
+val = 3**3 #exponent
+print(val)
+
+var = (2+3) *5
+print(var)
+
+float_var= 0.2+0.9
+print(float_var)
+
+python= 3*0.1
+print(python)
+
+a,b,c = 10,20,30
+print("a = " ,a)
+print("b = " ,b)
+print("c = " ,c)
+
+MAX_CUSTOMERS=500 #const value if written in capital letter
+print(MAX_CUSTOMERS)
+
