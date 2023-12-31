31a21b9be707d56e47c6f05d0565a5f803ad9bd8
Ali Akber <94167678+aliakberi41@users.noreply.github.com>
2023-12-31 07:09:08 +0500

    Add files via upload

---
+++ if-else-else-if.py
@@ -0,0 +1,20 @@
+isLate=True
+if isLate:
+    print("I need to call my BOSS")
+
+car_model="BMW"
+car_year=2015
+if (2023-car_year) <10 :
+    print(f"Your car is {2023-car_year} years old and its almost new we can buy it")
+else:
+    print(f"Sorry our company doesn't buy {car_model} cars that age is more than 10")
+    print(f"Your car age is {2023-car_year}")
+
+car_makers=["Mercedes","Bmw","VW","HONDA","KIA","Hyundai"]
+for car in car_makers:
+ if car == "Bmw" :
+    print(car.upper())
+ else:
+    print(car.title())
+
+print
