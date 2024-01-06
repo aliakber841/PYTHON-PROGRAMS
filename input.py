ab24535ff3dfaa5fcf71a74f57d34def7a3a8d71
Ali Akber <94167678+aliakberi41@users.noreply.github.com>
2024-01-06 10:56:31 +0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,6 @@
+name= input("What is your name? ")
+print("Hi " + name)  #CONCATENATING
+
+person=input("What is the person name ")
+color=input("What is his favorite color ")
+print(person + " likes " + color)
---
+++ type-conversions.py
@@ -0,0 +1,9 @@
+birth_year=input("Birth Year: ")
+print(type(birth_year))
+age=2024-int(birth_year)
+print(type(age))
+print(age)
+
+weight_in_pounds=input("Write your weight in pounds")
+weight_in_kilograms=int(weight_in_pounds) * 0.45
+print(weight_in_kilograms)
