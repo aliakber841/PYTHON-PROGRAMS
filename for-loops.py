2057ad0512a76f4669c1fa608aae4b0912ab0d90
Ali Akber <94167678+aliakberi41@users.noreply.github.com>
2024-02-16 10:32:15 +0500

    Add files via upload

---
+++ for-loops.py
@@ -0,0 +1,17 @@
+for item in 'Python' :
+ print(item)
+ 
+for items in ['Ali','Bilal','Hassan']:
+ print(items)
+ 
+for numbers in [1,2,3,4,5]:
+ print(numbers)
+ 
+for item2 in range(5,10,2):
+ print(item2)
+ 
+prices=[10,20,30]
+total=0
+for shopping in prices:
+   total=total+shopping
+print(f"Total:{total}")
---
+++ nested-loops.py
@@ -0,0 +1,10 @@
+for x in range(4):
+   for y in range(3):
+     print(f"({x},{y})")
+     
+numbers=[5,2,5,2,2]
+for digits in numbers:
+   output=''
+   for add in range(digits):
+      output+='x'
+   print(output)
