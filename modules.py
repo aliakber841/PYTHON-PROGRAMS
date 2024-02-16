ec2a5e06d8c0e767046fedb4f5d2e97a740f282a
Ali Akber <94167678+aliakberi41@users.noreply.github.com>
2024-02-16 19:30:17 +0500

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,9 @@
+def lbs_to_kg(weight):
+    return weight*0.45
+
+def kg_to_lbs(weight):
+    return weight/0.45
+
+# We can make a separate file of these functions and then import them in any file It better organize our code. For Example
+#import converters
+# print(converters.kg_to_lbs(70))
